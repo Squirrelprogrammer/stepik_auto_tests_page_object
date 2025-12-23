ddc8376f9f216176af205c410706f007298f6ea9
Hashper.B <hashper.b@reid21.ru>
2025-12-23 11:10:15 +0500

    Проверка существования страницы авторизации в тесте главной страницы

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,7 +10,7 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # проверка на корректный url адрес
-        assert "login" in self.browser.current_url(), f"'login' is not found in current url"
+        assert "login" in self.browser.current_url, f"'login' is not found in current url"
 
     def should_be_login_form(self):
         # проверка, что есть форма логина
