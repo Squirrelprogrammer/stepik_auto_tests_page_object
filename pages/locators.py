7ccb1edc8274bef69eefe2940a38b5b49745b04c
Hashper.B <hashper.b@reid21.ru>
2025-12-24 10:42:38 +0500

    Добавлен тест добавления товара в корзину

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,11 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CLASS_NAME, 'login_form')
     REGISTER_FORM = (By.CLASS_NAME, 'register_form')
+
+
+class ProductPageLocators:
+    BTN_ADD_TO_BASKET = (By.CLASS_NAME, 'add-to-basket')
+    PRODUCT_NAME = (By.TAG_NAME, 'h1')
+    PRODUCT_PRICE = ('css selector', 'p.price_color')
+    PRODUCT_NAME_IN_BASKET = (By.CSS_SELECTOR, ".alertinner strong")
+    PRODUCT_PRICE_IN_BASKET = (By.CSS_SELECTOR, ".alertinner p strong")
---
+++ pages/product_page.py
@@ -0,0 +1,26 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+
+    def should_be_button_add_to_basket(self):
+        assert self.is_element_present(
+            *ProductPageLocators.BTN_ADD_TO_BASKET), "Button for adding to basket is not presented"
+
+    def add_product_to_basket(self):
+        add_button = self.browser.find_element(*ProductPageLocators.BTN_ADD_TO_BASKET)
+        add_button.click()
+        self.solve_quiz_and_get_code()
+
+    def check_product_name(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        product_name_in_basket = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_BASKET).text
+        assert product_name == product_name_in_basket,\
+            f"Product name '{product_name}' is no equal product name in basket '{product_name_in_basket}'"
+
+    def check_product_price(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        product_price_in_basket = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_IN_BASKET).text
+        assert product_price == product_price_in_basket, \
+            f"Product price '{product_price}' is no equal product name in basket '{product_price_in_basket}'"
